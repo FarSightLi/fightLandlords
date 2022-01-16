df3d3e89f946f4ff3d31da7b1595bcbd754ec17d
FaiSightLi <farsight@live.com>
2022-01-16 10:41:56 +0800

    只能运行在IDEA的斗地主，未来可能使用GUI和Web FIGHT LORDS only run in IDEA，use GUI and Web in future

---
+++ game1.1/src/main/java/FlowControl.java
@@ -0,0 +1,11 @@
+/**
+ * @version: V1.0
+ * @author: FarSight
+ * @className: FlowControl
+ * @packageName: PACKAGE_NAME
+ * @description: 游戏流程
+ * @data: 2022-01-02 15:33
+ **/
+
+public class FlowControl {
+}
---
+++ game1.1/src/main/java/Logic.java
@@ -0,0 +1,11 @@
+/**
+ * @version: V1.0
+ * @author: FarSight
+ * @className: Logic
+ * @packageName: PACKAGE_NAME
+ * @description: 交互逻辑
+ * @data: 2022-01-02 15:33
+ **/
+
+public class Logic {
+}
